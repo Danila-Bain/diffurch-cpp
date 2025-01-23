af4d34283cb9e69dbac79f6b73f359583050809a
Danila Bain <danila.bain@yandex.com>
2025-01-23 09:57:37 +0300

    SaveAll save handler added

--- src/events/handlers.hpp
+++ src/events/handlers.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
+#include "../symbolic/variables.hpp"
+#include <cstddef>
 #include <limits>
-
+#include <tuple>
 // Special Set Handler
 struct StopIntegration {
 
@@ -9,4 +11,14 @@ struct StopIntegration {
     state.t_prev = state.t_curr;
     state.t_curr = std::numeric_limits<double>::max();
   }
+};
+
+// Special Save Handler to save current time and state variables
+// Intended to be part of default event for solver
+template <typename Equation> auto SaveAll() {
+  static constexpr size_t n =
+      std::tuple_size<decltype(std::declval<Equation>.get_ic()(0.))>::value;
+
+  return std::tuple_cat(std::make_tuple(State::TimeVariable()),
+                        State::Variables<n>());
 };
