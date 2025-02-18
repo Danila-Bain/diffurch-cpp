a9cf88336c4d220a6100760dbbc601e971c1dde6
Danila Bain <danila.bain@yandex.com>
2025-02-18 15:22:12 +0300

    global header

---
+++ diffurch.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "src/equations.hpp"
+#include "src/events.hpp"
+#include "src/rk_tables.hpp"
+#include "src/solver.hpp"
+#include "src/state.hpp"
+#include "src/stepsize.hpp"
+#include "src/symbolic.hpp"
