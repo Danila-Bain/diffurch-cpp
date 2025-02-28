5badb7b1fe6616cfb17cd7cdcdd4eaf0b4f7e5cc
Danila Bain <danila.bain@yandex.com>
2025-02-28 12:46:14 +0300

    redefine lorenz equation with new syntax

--- src/equations/lorenz.hpp
+++ src/equations/lorenz.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "../solver.hpp"
+#include <tuple>
 namespace diffurch::equation {
 struct Lorenz : Solver<Lorenz> {
 
@@ -8,18 +9,22 @@ struct Lorenz : Solver<Lorenz> {
   Lorenz(double sigma_ = 10., double rho_ = 28., double beta_ = 8. / 3.)
       : sigma(sigma_), rho(rho_), beta(beta_) {};
 
-  static constexpr auto t = TimeVariable();
-  static constexpr auto x = Variable<0>();
-  static constexpr auto y = Variable<1>();
-  static constexpr auto z = Variable<2>();
+  /*static constexpr auto t = TimeVariable();*/
+  /*static constexpr auto x = Variable<0>();*/
+  /*static constexpr auto y = Variable<1>();*/
+  /*static constexpr auto z = Variable<2>();*/
 
   static const bool ic_is_true_solution = false;
 
   auto get_lhs() {
-    return sigma * (x - y) | x * (rho - z) - y | x * y - beta * z;
+    using namespace diffurch::variables_xyz_t;
+    return sigma * (y - x) | x * (rho - z) - y | x * y - beta * z;
   }
-  auto get_ic() { return Vector(); }
+  auto get_ic() { return Constant(1.1) | 1.2 | 20.; }
 
-  auto get_events() { return Events(StepEvent(std::make_tuple(t, x, y, z))); }
+  auto get_events() {
+    using namespace diffurch::variables_xyz_t;
+    return std::make_tuple(StepEvent(t | x | y | z));
+  }
 };
 } // namespace diffurch::equation
