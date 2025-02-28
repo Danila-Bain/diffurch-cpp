8fa9762ce37752c0a78396008353bfe93ac84672
Danila Bain <danila.bain@yandex.com>
2025-02-28 12:48:32 +0300

    global_error.hpp order of function definition fix

--- src/test/global_error.hpp
+++ src/test/global_error.hpp
@@ -5,20 +5,9 @@
 
 #include "../util/print.hpp"
 #include <iostream>
+#include <tuple>
 
 namespace diffurch::test {
-template <typename RK = rk98>
-std::vector<double> global_error(auto equation, double initial_time,
-                                 double final_time,
-                                 const std::vector<double> &stepsizes) {
-
-  std::vector<ConstantStepsize> stepsize_controllers(stepsizes.size());
-  std::transform(stepsizes.begin(), stepsizes.end(),
-                 stepsize_controllers.begin(),
-                 [](double stepsize) { return ConstantStepsize(stepsize); });
-  return global_error<RK>(equation, initial_time, final_time,
-                          stepsize_controllers);
-}
 
 template <typename RK = rk98, typename StepsizeController>
 std::vector<double>
@@ -38,23 +27,38 @@ global_error(auto equation, double initial_time, double final_time,
   std::array<double, n> true_x = true_solution(final_time);
   /*cout << true_solution(t) << endl;*/
   std::vector<double> error(stepsize_controllers.size());
-  std::transform(stepsize_controllers.begin(), stepsize_controllers.end(),
-                 error.begin(), [&](StepsizeController stepsize_controller) {
-                   auto res = equation.template solution<RK>(
-                       initial_time, final_time, stepsize_controller,
-                       Events(StopEvent(SaveAll<decltype(equation)>())));
-                   double t;
-                   std::array<double, n> x;
-                   std::apply(
-                       [&](auto t_, auto... x_i) {
-                         t = t_[0];
-                         x = std::array<double, sizeof...(x_i)>({x_i[0]...});
-                       },
-                       res);
+  std::transform(
+      stepsize_controllers.begin(), stepsize_controllers.end(), error.begin(),
+      [&](StepsizeController stepsize_controller) {
+        auto res = equation.template solution<RK>(
+            initial_time, final_time, stepsize_controller,
+            std::make_tuple(StopEvent(SaveAll<decltype(equation)>())));
+        double t;
+        std::array<double, n> x;
+        std::apply(
+            [&](auto t_, auto... x_i) {
+              t = t_[0];
+              x = std::array<double, sizeof...(x_i)>({x_i[0]...});
+            },
+            res);
 
-                   /*cout << res << endl;*/
-                   return norm(x - true_x) / (1 + norm(true_x));
-                 });
+        /*cout << res << endl;*/
+        return norm(x - true_x) / (1 + norm(true_x));
+      });
   return error;
 }
+
+template <typename RK = rk98>
+std::vector<double> global_error(auto equation, double initial_time,
+                                 double final_time,
+                                 const std::vector<double> &stepsizes) {
+
+  std::vector<ConstantStepsize> stepsize_controllers(stepsizes.size());
+  std::transform(stepsizes.begin(), stepsizes.end(),
+                 stepsize_controllers.begin(),
+                 [](double stepsize) { return ConstantStepsize(stepsize); });
+  return global_error<RK>(equation, initial_time, final_time,
+                          stepsize_controllers);
+}
+
 } // namespace diffurch::test
