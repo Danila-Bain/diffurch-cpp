fef92201b3f07774f6fdbf8aa5bca24f2f29eb73
Danila Bain <danila.bain@yandex.com>
2025-03-01 10:01:33 +0300

    sign function with crossing detection implementation and testing

--- src/symbolic/piecewise.hpp
+++ src/symbolic/piecewise.hpp
@@ -1,31 +1,79 @@
 #pragma once
 
 #include "expression.hpp"
+#include <tuple>
 
 namespace diffurch {
 
-/*template <IsStateExpression Arg> struct Function_sign : StateExpression {*/
-/*  Arg arg;*/
-/*  Function_sign(Arg arg_) : arg(arg_) {}*/
-/*  double value;*/
-/*  auto operator()(const auto &state) const { return value; }*/
-/*  auto operator()(const auto &state, double t) const {*/
-/*    return sign(arg(state, t));*/
-/*  }*/
-/*  auto operator()(double t) const { return sign(arg(t)); }*/
-/*  auto prev(const auto &state) const { return value; }*/
-/**/
-/*  auto get_events() const {*/
-/*    return Events(*/
-/*        arg.get_events(),*/
-/*        StartEvent(nullptr,*/
-/*                   [this](const auto &state) { value = sign(arg(state)); }),*/
-/*        Event(arg == 0, nullptr, [this](auto &state) { value = -value; }));*/
-/*  }*/
-/*};*/
-/*template <IsStateExpression Arg> auto sign(Arg arg) {*/
-/*  return Function_sign(arg);*/
-/*}*/
+template <typename T> T sign(T x) { return (x > 0) - (x < 0); }
+
+template <IsStateExpression Arg> struct Function_sign : StateExpression {
+  Arg arg;
+  Function_sign(Arg arg_) : arg(arg_) {}
+  double curr_value = 0;
+  auto operator()(const auto &state) const { return curr_value; }
+  auto operator()(const auto &state, double t) const {
+    return sign(arg(state, t));
+  }
+  auto operator()(double t) const { return sign(arg(t)); }
+  auto prev(const auto &state) const { return curr_value; }
+
+  auto get_events() {
+    return std::tuple_cat(
+        arg.get_events(),
+        std::make_tuple(StartEvent(nullptr,
+                                   [this](const auto &state) {
+                                     std::cout << "start event called"
+                                               << std::endl;
+                                     curr_value = sign(arg(state));
+                                   }),
+                        Event(When(arg == 0), nullptr, [this](auto &state) {
+                          curr_value = -curr_value;
+                        })));
+  }
+};
+template <IsStateExpression Arg> auto sign(Arg arg) {
+  return Function_sign(arg);
+}
+
+template <IsStateExpression Arg> struct Function_step : StateExpression {
+  Arg arg;
+  double low_value;
+  double high_value;
+  double threshold;
+
+  Function_step(Arg arg_, double low = 0, double high = 1, double thresh = 0)
+      : arg(arg_), low_value(low), high_value(high), threshold(thresh) {}
+
+  double curr_value = 0;
+
+  auto operator()(const auto &state) const { return curr_value; }
+  auto operator()(const auto &state, double t) const {
+    return arg(state, t) < threshold ? low_value : high_value;
+  }
+  auto operator()(double t) const {
+    return arg(t) < threshold ? low_value : high_value;
+  }
+  auto prev(const auto &state) const { return curr_value; }
+
+  auto get_events() {
+    return std::tuple_cat(
+        arg.get_events(),
+        std::make_tuple(
+            StartEvent(nullptr,
+                       [this](const auto &state) {
+                         curr_value =
+                             arg(state) < threshold ? low_value : high_value;
+                       }),
+            Event(When(arg == threshold), nullptr, [this](auto &state) {
+              arg(state) < threshold ? low_value : high_value;
+            })));
+  }
+};
+template <IsStateExpression Arg> auto step(Arg arg, auto... args) {
+  return Function_step(arg, args...);
+}
+
 /**/
 /*template <IsStateExpression Arg> struct Function_abs : StateExpression {*/
 /*  Arg arg;*/
---
+++ test/piecewise.cpp
@@ -0,0 +1,49 @@
+#include "../diffurch.hpp"
+#include "../src/equations.hpp"
+#include "../src/rk_tables/rktp64.hpp"
+#include "../src/test/global_error.hpp"
+#include "../src/util/print.hpp"
+/*#include <iostream>*/
+/*#include <cmath>*/
+/*#include <matplot/matplot.h>*/
+
+#include <matplotlibcpp.h>
+#include <numpy/arrayobject.h>
+#include <tuple>
+
+namespace plt = matplotlibcpp;
+using namespace diffurch;
+using namespace variables_x_t;
+
+struct SignRHS : Solver<SignRHS> {
+
+  double c;
+  double d;
+  double x0;
+
+  SignRHS(double c_, double d_, double x0_) : c(c_), d(d_), x0(x0_) {};
+
+  auto get_rhs() { return Vector(c + d * sign(x)); }
+  auto get_ic() { return Vector(Constant(x0)); } // x(0) = x0
+};
+
+int main(int, char *[]) {
+
+  {
+    auto eq = SignRHS(2., 1., -1.);
+    auto sol = eq.solution(
+        -1, 1, ConstantStepsize(0.3),
+        std::tuple_cat(std::make_tuple(StepEvent(t | x | sign(x(t))))));
+
+    std::cout << sol << std::endl; //
+  }
+  {
+    auto eq = SignRHS(-2., -1., 1.);
+    auto sol = eq.solution(
+        -1, 1, ConstantStepsize(0.3),
+        std::tuple_cat(std::make_tuple(StepEvent(t | x | sign(x)))));
+
+    std::cout << sol << std::endl; //
+  }
+  return 0;
+}
